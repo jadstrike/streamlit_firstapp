8d086ce0b830493d346d9a313db6b1d437faedf0
Khant Zwe Naing <63854143+jadstrike@users.noreply.github.com>
2021-08-24 01:53:55 +0630

    Add files via upload

---
+++ firtsapp.py
@@ -0,0 +1,11 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+st.write("""
+# My firstapp
+Hello everyone! This is firsttime testing streamlit
+""")
+
+# https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
+#define the ticker symbol
